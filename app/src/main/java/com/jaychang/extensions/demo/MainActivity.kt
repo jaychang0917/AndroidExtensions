851264df0270604ac4fbe943d803ab24e7025edd
jay <jaychang0917@gmail.com>
2017-11-08 01:03:45 +0800

    Add round views

---
+++ app/src/main/java/com/jaychang/extensions/demo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jaychang.extensions.demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jaychang.android.extensions.R
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+}
