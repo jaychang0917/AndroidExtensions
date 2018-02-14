759296419f097b5e260d3eefd7d0cf4502aba15d
jay <jay.chang@redso.com.hk>
2018-02-14 11:07:11 +0800

    no message

---
+++ library/src/main/java/com/jaychang/extensions/ui/View.kt
@@ -0,0 +1,27 @@
+package com.jaychang.extensions.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+import com.jaychang.extensions.internal.ViewHelper
+
+class View:  View{
+  private val viewHelper = ViewHelper()
+
+  constructor(ctx: Context) : this(ctx, null, 0)
+  constructor(ctx: Context, attrs: AttributeSet?) : this(ctx, attrs, 0)
+  constructor(ctx: Context, attrs: AttributeSet?, defaultStyle: Int) : super(ctx, attrs, defaultStyle) {
+    viewHelper.init(this, ctx, attrs)
+  }
+
+  override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+    viewHelper.onSizeChanged(width, height)
+  }
+
+  override fun draw(canvas: Canvas) {
+    viewHelper.clipPath(canvas)
+    super.draw(canvas)
+    viewHelper.drawPath(canvas)
+  }
+}
