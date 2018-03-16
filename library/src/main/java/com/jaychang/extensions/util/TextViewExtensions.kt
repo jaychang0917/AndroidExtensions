aab4fb061619f3b9b0266fb87e17648df4132be8
jay <jay.chang@redso.com.hk>
2018-03-16 11:49:34 +0800

    no message

--- library/src/main/java/com/jaychang/extensions/util/TextViewExtensions.kt
+++ library/src/main/java/com/jaychang/extensions/util/TextViewExtensions.kt
@@ -1,6 +1,7 @@
 package com.jaychang.extensions.util
 
 import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.widget.TextView
 
 val TextView.textBounds: Rect
@@ -8,4 +9,36 @@ val TextView.textBounds: Rect
     val bound = Rect()
     paint.getTextBounds(text.toString(), 0, text.length, bound)
     return bound
+  }
+
+var TextView.leftDrawable: Drawable
+  get() {
+    return compoundDrawables[0]
+  }
+  set(value) {
+    setCompoundDrawablesWithIntrinsicBounds(value, topDrawable, rightDrawable, bottomDrawable)
+  }
+
+var TextView.topDrawable: Drawable
+  get() {
+    return compoundDrawables[1]
+  }
+  set(value) {
+    setCompoundDrawablesWithIntrinsicBounds(leftDrawable, value, rightDrawable, bottomDrawable)
+  }
+
+var TextView.rightDrawable: Drawable
+  get() {
+    return compoundDrawables[2]
+  }
+  set(value) {
+    setCompoundDrawablesWithIntrinsicBounds(leftDrawable, topDrawable, value, bottomDrawable)
+  }
+
+var TextView.bottomDrawable: Drawable
+  get() {
+    return compoundDrawables[3]
+  }
+  set(value) {
+    setCompoundDrawablesWithIntrinsicBounds(leftDrawable, topDrawable, rightDrawable, value)
   }
