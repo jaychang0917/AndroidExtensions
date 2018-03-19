13eaf2529577d323ad8d5dec6324f5b3e0b9dc53
jay <jay.chang@redso.com.hk>
2018-03-19 12:52:59 +0800

    disable rx audio

--- library/src/main/java/com/jaychang/extensions/av/AudioRecorder.kt
+++ library/src/main/java/com/jaychang/extensions/av/AudioRecorder.kt
@@ -1,32 +1,32 @@
-package com.jaychang.extensions.av
-
-import android.media.MediaRecorder
-import com.github.piasy.rxandroidaudio.AudioRecorder
-import java.io.File
-
-object AudioRecorder {
-  private val recorder: AudioRecorder = AudioRecorder.getInstance()
-
-  var onError: ((Int) -> Unit)? = null
-    set(value) {
-      recorder.setOnErrorListener {
-        value?.invoke(it)
-      }
-    }
-
-  fun prepare(file: File,
-              source: Int = MediaRecorder.AudioSource.MIC,
-              format: Int = MediaRecorder.OutputFormat.MPEG_4,
-              encoder: Int = MediaRecorder.AudioEncoder.AAC
-  ) {
-    recorder.prepareRecord(source, format, encoder, file)
-  }
-
-  fun start() {
-    recorder.startRecord()
-  }
-
-  fun stop() {
-    recorder.startRecord()
-  }
-}
+//package com.jaychang.extensions.av
+//
+//import android.media.MediaRecorder
+//import com.github.piasy.rxandroidaudio.AudioRecorder
+//import java.io.File
+//
+//object AudioRecorder {
+//  private val recorder: AudioRecorder = AudioRecorder.getInstance()
+//
+//  var onError: ((Int) -> Unit)? = null
+//    set(value) {
+//      recorder.setOnErrorListener {
+//        value?.invoke(it)
+//      }
+//    }
+//
+//  fun prepare(file: File,
+//              source: Int = MediaRecorder.AudioSource.MIC,
+//              format: Int = MediaRecorder.OutputFormat.MPEG_4,
+//              encoder: Int = MediaRecorder.AudioEncoder.AAC
+//  ) {
+//    recorder.prepareRecord(source, format, encoder, file)
+//  }
+//
+//  fun start() {
+//    recorder.startRecord()
+//  }
+//
+//  fun stop() {
+//    recorder.startRecord()
+//  }
+//}
